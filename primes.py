b33711888a81931d0e202f07e77b19dd54ba0f21
ayla <k22011048@kcl.ac.uk>
2023-10-02 15:23:23 +0100

    Add Solution

--- primes.py
+++ primes.py
@@ -2,5 +2,29 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    init = 2
     list = []
+
+    def isPrime(val):
+        if val==1:
+            return False
+        elif num > 1:
+            for i in range (2, val):
+                if val%i == 0:
+                    return False
+        return True
+
+    if number_of_primes<1:
+        raise ValueError("Must be >0 ")
+    if number_of_primes ==1:
+        list.append(init)
+        return  list
+    else:
+        list.append(init)
+        while len(list)<number_of_primes:
+            init +=1
+            if isPrime(init):
+                list.append(init)
+    print(list)
+
     return list
